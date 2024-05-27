0e7dee86b8ab54f8829f2791eeab7c6496afa367
hskim <fbhydfa@gmail.com>
2024-05-27 20:39:22 +0900

    이미지 api url 변경

--- HODU/js/hodu.js
+++ HODU/js/hodu.js
@@ -5,7 +5,7 @@ addImage.addEventListener('click', addToImg);
 
 function addToImg(){
     const container = document.querySelector('.add-div');
-    const imgUrl = `https://picsum.photos/200/300?random=`;
+    const imgUrl = `https://picsum.photos/200/300?random=`; //
     let number = Math.floor(Math.random() * 1000000); // 중복 제거 로직 추가 해야할 것.
 
     for(let i=1; i < 3; i++){
